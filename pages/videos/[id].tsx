c05a728a207bd78f1c71cf27ddc6f0b834d9f4d9
moon <moon@glocalizeinc.com>
2022-03-14 10:56:57 +0900

    update meta tag

--- pages/videos/[id].tsx
+++ pages/videos/[id].tsx
@@ -1,4 +1,5 @@
 import { GetStaticPaths, GetStaticProps } from "next"
+import Head from "next/head"
 import Card from "../../components/card"
 import { getAllVideos, getVideoData } from "../../lib/videos"
 import { IVideo, IVideos } from "../../types"
@@ -6,6 +7,16 @@ import { IVideo, IVideos } from "../../types"
 export default function Video(props: IVideo) {
   return (
     <div>
+      <Head>
+        <title>{props.title}</title>
+        <meta name="description" content={props.description.slice(0, 60)} />
+        <meta name="keywords" content={`침착맨, 유튜브, 한글, 번역, ${props.title}`} />
+        <meta name="og:site_name" content="gloz" />
+        <meta name="og:title" content={props.title} />
+        <meta name="og:description" content={props.description.slice(0, 60)} />
+        <meta name="og:url" content="https://gloz.io" />
+        <meta name="og:image" content={props.thumbnail.url} />
+      </Head>
       <Card item={props} />
     </div>
   )
--- pages/videos/index.tsx
+++ pages/videos/index.tsx
@@ -1,4 +1,5 @@
 import { GetStaticProps } from "next";
+import Head from "next/head";
 import Card from "../../components/card";
 import { getAllVideos } from "../../lib/videos";
 import { IVideos } from "../../types";
@@ -9,6 +10,16 @@ export default function Videos({
 }: IVideos) {
   return (
     <div>
+      <Head>
+        <title>침착맨 유튜브 번역 검색 결과</title>
+        <meta name="description" content="번역된 침착맨의 영상 목록입니다" />
+        <meta name="keywords" content="침착맨, 유튜브, 검색, 번역" />
+        <meta name="og:site_name" content="gloz" />
+        <meta name="og:title" content="침착맨 유튜브 번역 검색 결과" />
+        <meta name="og:description" content="번역된 침착맨의 영상 목록입니다" />
+        <meta name="og:url" content="https://gloz.io" />
+        <meta name="og:image" content={items[0].thumbnail.url} />
+      </Head>
       <h1>{region}</h1>
       <div>
         {items.map((item, index) => (
